27cd8bfe6212e079cb389aa41ebdd4667e7ac2b3
evan-cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-03-25 03:05:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Evan Cagiannos
+// Created on: 03 24 2022
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
